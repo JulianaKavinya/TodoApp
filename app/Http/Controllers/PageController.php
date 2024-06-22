d17b732d1e7653586bffbf40e07186d28de45df0
Juliana Kavinya <kavinyajuliana990@gmail.com>
2024-06-22 15:36:15 +0300

    first assignment in laravel project

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -17,4 +17,19 @@ class PageController extends Controller
     public function contact(){
         return 'this is the contact page';
     }
+    public function blog(){
+        return 'this is the blog page';
+    }
+
+    public function services(){
+        return 'this is the services page';
+    }
+
+    public function products(){
+        return 'this is the products page';
+    }
+
+    public function gallery(){
+        return 'this is the gallery page';
+    }
 }
